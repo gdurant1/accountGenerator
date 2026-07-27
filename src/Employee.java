b23804e6c9e5bab3c9bc62640d86104ac90d9c1d
Durant <gunnar.durant@protonmail.com>
2026-07-26 22:31:15 -0700

    module 10

--- src/Employee.java
+++ src/Employee.java
@@ -1,20 +1,45 @@
 import java.util.Scanner;
-
+// JB 1-b create employee class
 public class Employee {
     private String name;
     private String username;
     private String email;
     private String password;
 
-    //JP d constructor
+    Scanner scanner = new Scanner(System.in);
+
+    //JP 1-d constructor
     public Employee(){
         name = setName();
         username = setUserName(name);
         email = setEmail(username);
         password = setPassword(username);
+    }// end constructor
+
+    //JP 1-e private methode countChars
+    private int countChars(String str, char c){
+        int count = 0;
+        for (int i = 0; i < str.length(); i++){
+            if (str.charAt(i) == c){
+                count ++;
+            }
+        }
+        return count;
     }
 
-    // JP Create toString method
+    //JP 1-f create setName method
+    public String setName(){
+        String employeeName:
+        int nameCount =0;
+
+        do{
+            System.out.println("Employee full name (First Last): ");
+            employeeName = scanner.nextLine();
+
+            if
+    }
+
+    // JP 1-c Create toString method
     @Override
     public String toString(){
         return "Employee Details\n" +
