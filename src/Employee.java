a15b61973ce1db55893bdf818ec0c0466c3ab80b
Durant <gunnar.durant@protonmail.com>
2026-07-26 22:16:24 -0700

    module 10

--- src/Employee.java
+++ src/Employee.java
@@ -1,2 +1,18 @@
+import java.util.Scanner;
+
 public class Employee {
+    private String name;
+    private String username;
+    private String email;
+    private String password;
+
+    // JP Create toString method
+    @Override
+    public String toString(){
+        return "Employee Details\n" +
+                "Name            : " + name + "\n" +
+                "Username        : " + username + "\n" +
+                "Email           : " + email + "\n" +
+                "Initial Password: " + password;
+    }
 }
